d86b7e149ac234d01a0b5b7ec06c9815a6207acd
janhavism17 <janhavismupde@gmail.com>
2025-06-06 19:08:49 +0530

    Add files via upload

---
+++ Assignment18/asg18.5.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int Arr[], int iLenght)
+{
+    int i = 0;
+    for(i = 0; i < iLenght; i++)
+    {
+        if(Arr[i] % 11 == 0)
+        {
+            printf(" %d ",Arr[i]);
+        }
+    }
+    printf("\n");
+}
+int main()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of element:");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements:\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+
+
+    }
+    Display( p , iSize);
+
+    free(p);
+
+    return 0;
+}
