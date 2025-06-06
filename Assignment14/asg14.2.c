51753a7222bbc67a411c192751ba37ec9fc3d379
janhavism17 <janhavismupde@gmail.com>
2025-06-06 19:07:11 +0530

    Add files via upload

---
+++ Assignment14/asg14.2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>More actions
+
+void Pattern(int iRow,int iCol)
+{
+    int i = 0 ,j = 0;
+    int iCnt = 0;
+    
+
+    for(i = 1,iCnt= 1; i <= iRow ; i++)
+    {
+        for(j = 1; j <= iCol;j++,iCnt++)
+        {
+            if(i % 2 != 0)
+            {
+               if(iCnt % 2 == 0)
+               {
+                printf("%d\t",iCnt);
+               }
+            }
+            else if (i % 2 == 0)
+            {
+                printf("%d\t",iCnt);
+                
+            }
+            
+        }
+        printf("\n");
+    }
+
+}
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter Number of Row:");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Colums :");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+}
